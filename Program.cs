762673fbb3789475dd718b3b30818ee8d3409591
Elis <eliso1190@yandex.ru>
2023-02-14 00:34:54 +0300

    First

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int rows = GetNum("Введите количество строк: ");
+int columns = GetNum("Введите количество столбцов: ");
+int[,] matrix = new int[rows, columns];
+int[,] matrix2 = new int[rows, columns];
+int[,] matrix3 = new int[rows, columns];
+
+ShowMatrix(matrix);
+GenerateMatrix(matrix);
+
+Console.WriteLine();
+
+ShowMatrix(matrix2);
+GenerateMatrix(matrix2);
+
+Console.WriteLine();
+
+if (matrix.GetLength(0) != matrix2.GetLength(1))
+{
+    Console.WriteLine(" Нельзя перемножить ");
+    return;
+}
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix2.GetLength(1); j++)
+    {
+        matrix3[i, j] = 0;
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            matrix3[i, j] += matrix[i, k] * matrix2[k, j];
+        }
+    }
+}
+
+GenerateMatrix(matrix3);
+
+
+
+// Функция ввода
+int GetNum(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Функция заполнения массива рандомными числами от 1 до 9
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+// Функция вывода двумерного массива в терминал 
+void GenerateMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
